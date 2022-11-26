dfcf74106deaaff1323ec7afc2baa6bef98c6b8e
jieromu <jerome.leong@gmail.com>
2022-11-26 08:55:02 +0000

    Update 2 files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,70 @@
+import react from "react";
 import "./styles.css";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <Game />
     </div>
   );
+}
+
+class Square extends react.Component {
+  render(){
+    return (
+      <button className="square">
+        {/* TODO */}
+      </button>
+    );
+  }
+}
+
+class Board extends react.Component {
+  
+  renderSquare(i) {
+    return <Square />;
+  }
+  
+  render() {
+    
+    const status = "Next Player: X";
+    
+    return (
+      <div>
+        <div className="displayStatus">{status}</div>
+        <div className="board-row-1">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="board-row-2">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>  
+        <div className="board-row-3">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+      </div>
+    );
+  }
+}
+
+class Game extends react.Component {
+  
+  render() {
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board />
+        </div>
+        <div className="game-info">
+          <div>{/* For Status Update*/}</div>
+          <ol>{/* ToDO */}</ol>
+        </div>
+      </div>
+    );
+  }
 }
