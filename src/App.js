d1b3ea8e6e60e64658d63a420bc27f52c3e46058
jieromu <jerome.leong@gmail.com>
2022-11-26 10:56:15 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,40 +9,63 @@ export default function App() {
   );
 }
 
-class Square extends react.Component {
-  render(){
-    return (
-      <button className="square">
-        {/* TODO */}
+function Square(props) {
+  return (
+      <button 
+        className="square"
+        style={{ height: 50, width: 50, marginTop: 5}}
+        onClick={props.onClick}>
+          {props.value}
       </button>
-    );
-  }
+  );
 }
 
 class Board extends react.Component {
   
+  constructor(props){
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+      xIsNext: true,
+    };
+  }
+  
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    squares[i] = this.state.xIsNext ? 'X':'O';
+    this.setState({
+        squares: squares,
+        xIsNext: !this.state.xIsNext,
+      });
+  }
+  
   renderSquare(i) {
-    return <Square />;
+    return (
+      <Square 
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+      />
+    );
   }
   
   render() {
     
-    const status = "Next Player: X";
+    const status = "Next Player: " + (this.state.xIsNext ? 'X':'O');
     
     return (
       <div>
         <div className="displayStatus">{status}</div>
-        <div className="board-row-1">
+        <div className="board-row">
           {this.renderSquare(0)}
           {this.renderSquare(1)}
           {this.renderSquare(2)}
         </div>
-        <div className="board-row-2">
+        <div className="board-row">
           {this.renderSquare(3)}
           {this.renderSquare(4)}
           {this.renderSquare(5)}
         </div>  
-        <div className="board-row-3">
+        <div className="board-row">
           {this.renderSquare(6)}
           {this.renderSquare(7)}
           {this.renderSquare(8)}
