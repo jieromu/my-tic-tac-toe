32703bb5bdde77b300c4357acf465c008b5a4a46
jieromu <jerome.leong@gmail.com>
2022-11-27 06:28:48 +0000

    Includes changes to determine winner, and for click handler to ignore click if a square has been filled or there is a winner being declared.

--- src/App.js
+++ src/App.js
@@ -31,6 +31,9 @@ class Board extends react.Component {
   
   handleClick(i) {
     const squares = this.state.squares.slice();
+    if(determineWinner(squares) || squares[i]){
+      return;
+    }
     squares[i] = this.state.xIsNext ? 'X':'O';
     this.setState({
         squares: squares,
@@ -43,13 +46,20 @@ class Board extends react.Component {
       <Square 
         value={this.state.squares[i]}
         onClick={() => this.handleClick(i)}
-      />
+      />     
     );
   }
   
   render() {
     
-    const status = "Next Player: " + (this.state.xIsNext ? 'X':'O');
+    const winner = determineWinner(this.state.squares);
+    let status;
+    if(winner){
+      status = "Winner is " + winner;
+    }
+    else{
+      status = "Next Player: " + (this.state.xIsNext ? 'X':'O');
+    }
     
     return (
       <div>
@@ -89,4 +99,25 @@ class Game extends react.Component {
       </div>
     );
   }
+}
+
+function determineWinner(squares) {
+  const lines = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6],    
+  ];
+  
+  for (let i=0; i<lines.length; i++){
+    const [a, b, c] = lines[i];
+    if(squares[a] && squares[a] === squares[b] && squares[b] === squares[c]){
+      return squares[a];
+    }
+  }
+  return null;
 }
